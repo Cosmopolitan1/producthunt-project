f0da3cb33ccbd9979a7b8262b2649f18e947b3eb
Freddie Mercury <jampackraft@yahoo.com>
2019-10-03 09:41:51 -0400

    Completed Products detail and home page

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-import products.views
+from . import views
 
 urlpatterns = [
-	path('', products.views.home, name='home'),
+	path('create', views.create, name='create'),
+	path('<int:product_id>/', views.detail, name='detail'),
+	path('<int:product_id>/upvote', views.upvote, name='upvote'),
 ]
